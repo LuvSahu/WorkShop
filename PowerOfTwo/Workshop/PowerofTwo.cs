8a7178c6ac1d388acd803d3303e1072627012ce4
Luv sahu <lksahu404@gmail.com>
2022-07-07 10:58:05 +0530

    [Luv SAHU] Power of Two

---
+++ PowerOfTwo/Workshop/PowerofTwo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    internal class PowerofTwo
+    {
+        public void poweroftwo()
+        {
+            Console.WriteLine("Enter the no : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while(num < 0 || num >= 31)
+            {
+                Console.WriteLine("Please enter the valid no ");
+                num = Convert.ToInt32((Console.ReadLine()));
+            }
+
+            for(int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
---
+++ PowerOfTwo/Workshop/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Workshop
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            PowerofTwo powerTwo = new PowerofTwo();
+            powerTwo.poweroftwo();
+        }
+    }
+}
